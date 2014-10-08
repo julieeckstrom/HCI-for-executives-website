1492cd705e88463de73c659b02d3c5abf3b91d87
Julie Eckstrom <julie.eckstrom@gmail.com>
2014-10-07 20:19:40 -0400

    added carousel to homework page

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,17 @@ Date.prototype.getWeek = function() {
 
 $(document).ready(function() {
 
+	// handles the carousel thumbnails
+	$('.carousel-thumb').click( function(){
+	    var id_selector = $(this).attr("id");
+	    console.log(id_selector);
+	    var id = id_selector.substr(id_selector.length -1);
+	    id = parseInt(id);
+	    $('#homework-carousel').carousel(id);
+	    $('[id^=carousel-selector-]').removeClass('selected');
+	    $(this).addClass('selected');
+	});
+
 /*
 	$(window).scroll(function() {
 	var window_top = $(window).scrollTop();
