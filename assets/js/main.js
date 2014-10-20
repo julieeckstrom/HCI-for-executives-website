ef2eb9458027dc93fd4db92831e7cd063812e5fd
Julie Eckstrom <julie.eckstrom@gmail.com>
2014-10-20 13:02:11 -0400

    fixed homework

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,12 @@ Date.prototype.getWeek = function() {
 $(document).ready(function() {
 
 	$('#homework-thumbs').click(function(){
-		if($('#homework-thumbs li').is(":hidden")) {
-			console.log('here1');
-			$('#homework-thumbs li').css('display', 'block');
-		} else {
-			console.log('here2');
-			$('#homework-thumbs li').css('display', 'none');
+		if($('#homework-thumbs-title').is(":visible")) {
+			if($('#homework-thumbs li').is(":hidden")) {
+				$('#homework-thumbs li').css('display', 'block');
+			} else {
+				$('#homework-thumbs li').css('display', 'none');
+			}
 		}
 	});
 
