a86a3b412ecee300565844c57e2c5d46a04e236b
Julie Eckstrom <julie.eckstrom@gmail.com>
2014-10-22 16:59:32 -0400

    testing another header stick

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,65 +85,22 @@ $(document).ready(function() {
 	    }
 	});
 
-		// Index of the currently 'active' section
-		var activeCache = null;
-
-		// Actual rendered height of a header element
-		var cloneHeight = function(){
-		    var $clone = $('<div class="clone"></div>').appendTo('body'),
-		        cloneHeight = $clone.outerHeight();
-		    $clone.remove();
-		    return cloneHeight;
-		}();
-
-		// Top offsets of each header
-		var offsets = [];
-
-		// Figure out which section is 'active'
-		var activeHeaderIndex = function(){
-		    var scrollTop = document.body.scrollTop;
-		    for ( var i = 0; i < offsets.length; i++ )
-		        if ( offsets[i] - cloneHeight > scrollTop )
-		            return Math.max( i - 1, 0 );
-		}
-
-		// Build the 'offsets' array
-		$('.date').each(function(i, obj){
-		    offsets.push( $(this).offset().top );
-		});
-
-		// Listen to scroll events
-		$(window).on('scroll', function(){
-		    var active = activeHeaderIndex(), 
-		        scroll = document.body.scrollTop,
-		        clone = $('.clone').length,
-		        $active = $('.date').eq(active),
-		        prevTitle = $('.date').eq(active - 1).text(),
-		        title = $active.text(),
-		        $fixed = $('.fixed');
-		    // Hide fixed header
-		    if ( offsets[active] > scroll ){
-		        if ( !clone ){
-		            $('.date').eq(0).hide();
-		            $('<li class="clone">' + prevTitle + '</li>').insertBefore($active);
-		        }
-		        $fixed.hide();
-		    // Show fixed header
-		    } else {
-		        if ( clone ){
-		            $('.date').eq(0).show();
-		            $('.clone').remove();
-		        }
-		        $fixed.show();
-		    }
-		    // If we're not changing headers, exit
-		    if ( active == activeCache ) return;
-		    // Update active index
-		    activeCache = active;
-		    // Remove old fixed header (if any)
-		    $('.fixed').remove();
-		    // Add a new fixed header
-		    $fixed = $('<div class="fixed">' + title + '</div>').appendTo('body');
-		}).trigger('scroll');
+	$(document).scroll(function () {
+	    var sticky = $('#sticky'), h1 = $('.date:first');
+	    
+	    if (!sticky.length)
+	        sticky = $('<div id="sticky">').appendTo('body');
+	    
+	    $('.date').each(function () {
+	        if ($(this).parents('#sticky').length) return true;
+	        
+	        if ($(this).offset().top > $(window).scrollTop())
+	            return false; // Exit loop
+	        
+	        date = $(this);
+	    });
+	    
+	    sticky.empty().append(date.clone());
+	});
 
 });
