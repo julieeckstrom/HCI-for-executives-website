70ab2ecfb58cd893e39a8e8d391322c1f60ac1c6
Julie Eckstrom <julie.eckstrom@gmail.com>
2014-10-22 12:52:09 -0400

    still debuggin

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
 		var scrolledPlusViewable;
 		if(currentWeek > 43 && currentWeek < 52) {
 			console.log("here");
-			$('#today').css('display', 'visible');
+			$('#today').css('visibility', 'visible');
 			currentWeekDiv = $('#' + currentWeek);
 			pageTopToDivBottom  = $('#' + currentWeek).offset().top + $('#' + currentWeek)[0].scrollHeight;
 			scrolledPlusViewable = $(window).scrollTop()+$(window).height();
